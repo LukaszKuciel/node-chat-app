bcacdef11778212d694d1b181aa208c0da0da2b6
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2018-10-17 01:10:42 +0200

    Great new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,9 +13,17 @@ let io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('new user connected');
 
-    
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined the chat app',
+        createdAt: new Date().getTime()
+    })
 
     socket.on('createMessage', (msg) => {
         console.log('Create message', msg);
